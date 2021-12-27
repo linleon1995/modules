55c9954ffaa2490b4d4757f1f83c08d713f4f330
leonlin <l84328g@gmail.com>
2021-12-27 16:22:39 +0800

    note

--- train/trainer.py
+++ train/trainer.py
@@ -2,6 +2,7 @@ import os
 import torch
 import numpy as np
 from tensorboardX import SummaryWriter
+from engine import train_one_epoch, evaluate
 import sys
 sys.path.append("..")
 # TODO: improve metrics
@@ -54,7 +55,18 @@ class Trainer(object):
             self.train_writer.close()
             self.valid_writer.close()
             
+    
+
+
     def train(self):
+        for epoch in range(self.epoch):
+            # train for one epoch, printing every 10 iterations
+            train_one_epoch(self.model, self.optimizer, self.train_dataloader, self.device, self.epoch, print_freq=10)
+            # update the learning rate
+            # lr_scheduler.step()
+            # evaluate on the test dataset
+            evaluate(self.model, self.valid_dataloader, device=self.device)
+        
         self.model.train()
         print(60*"=")
         self.logger.info(f'Epoch {self.epoch}/{self.config.TRAIN.EPOCH}')
@@ -106,6 +118,59 @@ class Trainer(object):
         self.iterations = i
         if self.USE_TENSORBOARD:
             self.train_writer.add_scalar('Loss/epoch', total_train_loss/train_samples, self.epoch)
+
+    # def train(self):
+    #     self.model.train()
+    #     print(60*"=")
+    #     self.logger.info(f'Epoch {self.epoch}/{self.config.TRAIN.EPOCH}')
+    #     train_samples = len(self.train_dataloader.dataset)
+    #     for i, data in enumerate(self.train_dataloader, self.iterations + 1):
+    #         total_train_loss = 0.0
+    #         # batch_input, batch_target = data
+    #         # input_var = batch_input
+    #         # target_var = batch_target
+    #         # input_var, target_var = data['input'], data['gt']
+    #         input_var, target_var = data
+    #         input_var = train_utils.minmax_norm(input_var)
+    #         input_var, target_var = input_var.to(self.device), target_var.to(self.device)
+
+    #         # def closure():
+    #         batch_output = self.model(input_var)
+            
+    #         import matplotlib.pyplot as plt
+    #         # batch_output = self.activation_func(batch_output)
+    #         if i%20 == 0:
+    #             fig, ax = plt.subplots(1, 3)
+    #             ax[0].imshow(input_var.cpu().detach().numpy()[0,0], 'gray')
+    #             ax[0].imshow(target_var.cpu().detach().numpy()[0,0], cmap='jet', alpha=0.2)
+    #             ax[1].imshow(batch_output.cpu().detach().numpy()[0,0], 'gray')
+    #             ax[2].imshow(target_var.cpu().detach().numpy()[0,0], 'gray')
+    #             # plt.show()
+    #             fig.savefig(f'{i:03d}.png')
+
+    #         # if isinstance(self.criterion, torch.nn.CrossEntropyLoss):
+    #         #     loss = self.criterion(batch_output, torch.argmax(target_var.long(), axis=1))
+    #         # else:
+    #         #     loss = self.criterion(batch_output, target_var)
+    #         loss = self.criterion(batch_output, target_var)
+    #         loss.backward()
+    #             # return loss
+    #         self.optimizer.zero_grad()
+    #         self.optimizer.step()
+        
+    #         loss = loss.item()
+    #         total_train_loss += loss
+    #         if self.USE_TENSORBOARD:
+    #             self.train_writer.add_scalar('Loss/step', loss, i)
+
+    #         display_step = train_utils.calculate_display_step(num_sample=train_samples, batch_size=self.config.DATA.BATCH_SIZE)
+    #         # TODO: display_step = 10
+    #         display_step = 10
+    #         if i%display_step == 0:
+    #             self.logger.info('Step {}  Step loss {}'.format(i, loss))
+    #     self.iterations = i
+    #     if self.USE_TENSORBOARD:
+    #         self.train_writer.add_scalar('Loss/epoch', total_train_loss/train_samples, self.epoch)
 
     def validate(self):
         self.model.eval()
